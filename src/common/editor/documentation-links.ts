cac4cd1515afa99f7395a6482c98e684966608f8
Leo Wattenberg <48789553+LeoWattenberg@users.noreply.github.com>
2026-09-09 21:48:11 +0200

    fix(docs): link committed Japanese handbook pages

--- src/common/editor/documentation-links.ts
+++ src/common/editor/documentation-links.ts
@@ -27,7 +27,7 @@ export const DOCUMENTATION_BASE_URL = 'https://soundscaper.org/docs';
  */
 export const HANDBOOK_LANGUAGES: readonly string[] = Object.freeze([
 	'de', 'ar', 'cs', 'el', 'en-gb', 'es', 'fa',
-	'fi', 'fr', 'gl', 'he', 'hi', 'hy', 'id', 'it',
+	'fi', 'fr', 'gl', 'he', 'hi', 'hy', 'id', 'it', 'ja',
 ]);
 
 export type DocumentationDestination = 'manual' | 'tutorials';
