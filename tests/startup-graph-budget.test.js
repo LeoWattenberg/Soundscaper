121e91ce6307918721ab5e3b59ebc9ca34131d95
Leo Wattenberg <48789553+LeoWattenberg@users.noreply.github.com>
2026-09-25 00:57:56 +0200

    Budget merged Framescaper browser startup graph

--- tests/startup-graph-budget.test.js
+++ tests/startup-graph-budget.test.js
@@ -190,8 +190,8 @@ test('approved graph ceilings remain hard limits', () => {
 	});
 	assert.deepEqual(STARTUP_GRAPH_BUDGETS.framescaper, {
 		requests: 84,
-		rawBytes: 7_000_000,
-		brotliBytes: 1_675_500,
+		rawBytes: 7_003_000,
+		brotliBytes: 1_678_000,
 	});
 });
 
